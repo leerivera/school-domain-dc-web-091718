3036da0722f7aac1b270a0b92400c89f29194e93
Nick Rivera <leerivera80@gmail.com>
2018-09-24 16:50:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+
+# code here
+require 'pry'
+class School
+
+    attr_reader  :roster, :grade, :name
+  
+    
+    
+    def initialize(roster)
+      
+        @roster = {}
+    end
+     
+   
+    
+    def add_student(name, grade)
+      
+      
+      @name = name
+      @grade = grade
+      if @roster[grade] == nil
+        @roster[grade] = []
+      end
+     @roster[grade] << name 
+    end
+    
+    
+    def grade(grade)
+      @roster[grade]
+      
+      
+    end
+    
+    def sort
+      
+      @roster.values.sort
+      
+    end
+    
+end
+
+
+
+
